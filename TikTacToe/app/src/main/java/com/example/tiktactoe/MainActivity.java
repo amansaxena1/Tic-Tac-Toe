33eecd63c580063cc8158092ecbf9afdd763a8b9
amansaxena2001 <60064813+amansaxena2001@users.noreply.github.com>
2020-06-02 16:38:47 +0530

    The tic tac toe game

---
+++ TikTacToe/app/src/main/java/com/example/tiktactoe/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.tiktactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    boolean gameActive = true;
+    int activePlayer = 0;
+    int var = 0;
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winPositions ={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    public void playerTap(View view) {
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+        if (!gameActive) {
+            gameReset(view);
+        }
+        if (gameState[tappedImage] == 2) {
+            gameState[tappedImage] = activePlayer;
+            img.setTranslationX(-1000f);
+            if (activePlayer == 0) {
+                img.setImageResource(R.drawable.x);
+                activePlayer = 1;
+                TextView status = findViewById(R.id.status);
+                status.setText("O's Turn - Tap to play");
+            } else {
+                img.setImageResource(R.drawable.o);
+                activePlayer = 0;
+                TextView status = findViewById(R.id.status);
+                status.setText("X's Turn - Tap to play");
+            }
+            img.animate().translationXBy(1000f).setDuration(300);
+        }
+        for (int[] winPosition : winPositions) {
+            if (gameState[winPosition[0]] == gameState[winPosition[1]] && gameState[winPosition[1]] == gameState[winPosition[2]] && gameState[winPosition[0]] != 2) {
+                String winnerStr;
+                gameActive = false;
+                if (gameState[winPosition[0]] == 0) {
+                    winnerStr = "X has won";
+                } else {
+                    winnerStr = "O has won";
+                }
+                TextView status = findViewById(R.id.status);
+                status.setText(winnerStr);
+            }
+        }
+        gameDraw();
+        if(var == 1){
+            gameReset(view);
+            TextView status = findViewById(R.id.status);
+            status.setText("Draw");
+        }
+    }
+        public void gameReset (View view){
+            gameActive = true;
+            activePlayer = 0;
+            var = 0;
+            for(int i = 0; i < gameState.length; i++){
+                gameState[i] = 2;
+            }
+            ((ImageView)findViewById(R.id.imageView0)).setImageResource(0);
+            ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
+            ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
+            ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
+            ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
+            ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
+            ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
+            ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
+            ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
+
+            TextView status = findViewById(R.id.status);
+            status.setText("X's Turn - Tap to play");
+    }
+    public void gameDraw(){
+        if((gameState[0] != 2) && (gameState[1] != 2) && (gameState[2] != 2) && (gameState[3] != 2) && (gameState[4] != 2) && (gameState[5] != 2) && (gameState[6] != 2) && (gameState[7] != 2) && (gameState[8] != 2)){
+                var=1;
+                TextView status = findViewById(R.id.status);
+                status.setText("Draw");
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
